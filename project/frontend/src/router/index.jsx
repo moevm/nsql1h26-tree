66fce4cbd9cee1128eb0c0b67d608dad0f7da6db
Anna Moiseeva <144445136+Anna-Moiseeva-3341@users.noreply.github.com>
2026-04-20 20:53:48 +0300

    Update index.jsx

--- project/frontend/src/router/index.jsx
+++ project/frontend/src/router/index.jsx
@@ -2,14 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Search from "../pages/Search";
 import Placeholder from "../pages/Placeholder";
+import AddPerson  from "../pages/AddPerson";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/search", element: <Search /> },
-
   { path: "/graph", element: <Placeholder title="Граф" /> },
   { path: "/stats", element: <Placeholder title="Статистика" /> },
   { path: "/import", element: <Placeholder title="Импорт" /> },
   { path: "/export", element: <Placeholder title="Экспорт" /> },
-  { path: "/add", element: <Placeholder title="Добавление персоны" /> },
+  { path: "/add", element: <AddPerson /> },
 ]);
