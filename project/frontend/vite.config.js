d06f6d808947b9b2a36420db52e436e54968e480
annaeponil <annaeponil@gmail.com>
2026-05-03 16:21:25 +0300

    Update Vite configuration

--- project/frontend/vite.config.js
+++ project/frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // чтобы Vite был доступен снаружи контейнера
+    proxy: {
+      "/api": {
+        target: "http://backend:8000", // имя сервиса из docker-compose
+        changeOrigin: true,
+      },
+    },
+  },
+});
