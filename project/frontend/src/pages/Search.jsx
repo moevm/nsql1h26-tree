fe260a358e271bc3d9a24c08f78a6467229b9115
Настя Мильхерт <n.milkhert@gmail.com>
2026-05-05 06:31:28 +0300

    fix

--- project/frontend/src/pages/Search.jsx
+++ project/frontend/src/pages/Search.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import { searchPersons } from "../api/api";
+import { searchPersons, getPerson, deletePerson } from "../api/api";
 import Navbar from "../components/Navbar";
 import PersonModal from "../components/PersonModal";
 import "../style.css";
-import { getPerson } from "../api/api";
 
 
 export default function Search() {
@@ -120,8 +119,13 @@ export default function Search() {
           const full = await getPerson(id);
           setSelectedPerson(full);
         }}
-        onDelete={(id) => console.log("delete", id)}
-        onSave={(data) => console.log("save", data)}
+        onDelete={async (id) => {
+          if (!window.confirm("Вы уверены, что хотите удалить эту персону? Все связи будут удалены.")) return;
+          await deletePerson(id);
+          setIsModalOpen(false);
+          setSelectedPerson(null);
+          await search();
+        }}
       />
     </div>
   );
